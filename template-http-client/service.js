344b262dfb5034bfd99e8e16e5890a845e6b8c1e
ThuF <jordan.p.pavlov@gmail.com>
2017-11-03 12:51:16 +0200

    Updates template

--- template-http-client/service.js
+++ template-http-client/service.js
@@ -1,7 +1,7 @@
 var httpClient = require('http/v3/client');
 var response = require('http/v3/response');
 
-var httpResponse = httpClient.get('http://services.odata.org/V4/Northwind/Northwind.svc/', {'contentType': 'application/json'});
+var httpResponse = httpClient.get('http://services.odata.org/V4/Northwind/Northwind.svc/');
 
 response.println(httpResponse.text);
 response.flush();
